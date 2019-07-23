098ba031d42d57a3fd1ada3027da84f122d2af65
Plant Lady <codeplantsbylouie@gmail.com>
2019-07-23 18:30:30 -0400

    app js done; links created and nav bar done

--- src/App.js
+++ src/App.js
@@ -6,12 +6,35 @@ import TodosList from "./components/TodosList";
 import EditTodo from "./components/TodoEdit";
 import CreateTodo from "./components/TodoCreate";
 
+import logo from "./todologo.jpg";
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h2>MERN To Do App</h2>
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/" target="blank">
+              <img src={logo} width="30" height="30" alt="todo logo" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN Stack To Do App</Link>
+            <div className="collapse navbar-collapse">
+
+              <ul className="navbar-nav mr-auto">
+
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+                
+              </ul>
+            </div>
+          </nav>
+
 
           <Route path="/" exact component={TodosList} />
           <Route path="/edit/:id" component={EditTodo} />
